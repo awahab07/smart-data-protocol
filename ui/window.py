facbe52ea0f8adda877a752d83e1732a2fabdb01
azahid <abdul.zahid@jonasconstruction.com>
2018-07-02 01:46:39 +0500

    Drawn Gateway node with User Authorization and Access Control Component.

--- ui/window.py
+++ ui/window.py
@@ -7,10 +7,11 @@ from .packets import *
 
 winProps = {
     'title': "Smart Data - Access Control",
-    'width': 600,
+    'width': 900,
     'height': 700,
     'padding': 10,
-    'imageSize': 100
+    'imageSize': 100,
+    'fps': 1 / 60.0
 }
 
 winProps['xCenter'] = winProps['width'] / 2
@@ -22,6 +23,20 @@ window = pyglet.window.Window(winProps['width'], winProps['height'], winProps['t
 smartDataOwnerNode = Node('monitor.png', winProps['imageSize'], winProps['padding'], winProps['yCenter'], "Smart Data Owner")
 requesterNode = Node('monitor.png', winProps['imageSize'], winProps['xCenter'] - winProps['imageSize'] / 2, winProps['height'] - winProps['padding'] - winProps['imageSize'], "Smart Data Requester")
 cloudNode = Node('cloud.png', winProps['imageSize'], winProps['width'] - winProps['padding'] - winProps['imageSize'], winProps['yCenter'], "Cloud")
+
+# Initializing gateway node and associated authorization and access rule nodes
+gatewayNode = Node('gateway.png', winProps['imageSize'], winProps['xCenter'] - winProps['imageSize'] / 2, 2 * winProps['padding'] + winProps['imageSize'], "Gateway")
+authorizationServerNode = Node('authorization-server.png', winProps['imageSize'], winProps['xCenter'] - (winProps['imageSize'] * 2.5), winProps['padding'], "Authorization Server")
+userAccessRulesNode = Node('access-rule-component.png', winProps['imageSize'], winProps['xCenter'] + (winProps['imageSize'] * 1.5), winProps['padding'], "User Access Rule Component")
+
+# Initializing connecting lines
+connecting_lines_batch = pyglet.graphics.Batch()
+connecting_lines_batch.add(2, pyglet.gl.GL_LINES, None, ('v2f', (authorizationServerNode.getCenter()['x'], gatewayNode.getCenter()['y'], userAccessRulesNode.getCenter()['x'], gatewayNode.getCenter()['y'])))
+connecting_lines_batch.add(2, pyglet.gl.GL_LINES, None, ('v2f', (authorizationServerNode.getCenter()['x'], gatewayNode.getCenter()['y'], authorizationServerNode.getCenter()['x'], authorizationServerNode.getCenter()['y'] + 40)))
+connecting_lines_batch.add(2, pyglet.gl.GL_LINES, None, ('v2f', (userAccessRulesNode.getCenter()['x'], gatewayNode.getCenter()['y'], userAccessRulesNode.getCenter()['x'], userAccessRulesNode.getCenter()['y'] + 40)))
+
+def update_frames(dt):
+    pass
 
 @window.event
 def on_draw():
@@ -37,11 +52,32 @@ def on_draw():
     # Drawing cloudImage
     cloudNode.draw()
 
-    smartDataOwnerAuthRequest.transmit(smartDataOwnerNode, cloudNode)
+    # Drawing lines to connect gateway with authorization server and user access rules component
+    connecting_lines_batch.draw()
+
+    # Drawing gatewayNode
+    gatewayNode.draw()
+
+    # Drawing authorization server
+    authorizationServerNode.draw()
+
+    # Drawing user access rules component
+    userAccessRulesNode.draw()
+
+    # Drawing requests that will depict data transmission
+    drawPackets()
+
+    # Calling update frames every 60th of a second (Effectually creating a 60 fps).
+    pyglet.clock.schedule_interval(update_frames, winProps['fps'])
+
 
 @window.event
 def on_key_release(symbol, modifiers):
-    smartDataOwnerAuthRequest.transmit(smartDataOwnerNode, cloudNode)
+    if symbol == 49: # key "1"
+        smartDataOwnerAuthRequest.transmit(smartDataOwnerNode, cloudNode)
+    if symbol == 50: # key "1"
+        userAuthToken.transmit(cloudNode, smartDataOwnerNode)
+    pass
 
 def show():
     window.set_visible()
