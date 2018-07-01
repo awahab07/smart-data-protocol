64ff8c306f4683bae61c81863e143bf7b99e4ea7
azahid <abdul.zahid@jonasconstruction.com>
2018-07-02 01:48:42 +0500

    Added packet for User Auth Token (not traveling yet)

--- ui/packets.py
+++ ui/packets.py
@@ -8,7 +8,13 @@ from typing import List
 
 icons = {
     'authRequest': 'user-auth-request.png',
-    'userCertificate': 'certificate.png'
+    'userCertificate': 'certificate.png',
+    'userAuthToken': 'user-auth-token.png'
 }
 
 smartDataOwnerAuthRequest = Packet([icons['authRequest'], icons['userCertificate']])
+userAuthToken = Packet([icons['userAuthToken']])
+
+def drawPackets():
+    smartDataOwnerAuthRequest.draw()
+    userAuthToken.draw()
